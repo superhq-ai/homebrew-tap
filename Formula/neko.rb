a7d789fc476bb0c51394e01a93061c86faf2d8ce
harshdoesdev <harshsingh.js@gmail.com>
2026-07-30 14:23:30 +0530

    neko 0.2.1

--- Formula/neko.rb
+++ Formula/neko.rb
@@ -1,21 +1,21 @@
 class Neko < Formula
   desc "Expose a sandbox port to the web in one command"
   homepage "https://github.com/superhq-ai/neko-computer"
-  version "0.2.0"
+  version "0.2.1"
 
   depends_on arch: :arm64
 
   on_macos do
     on_arm do
       url "https://github.com/superhq-ai/neko-computer/releases/download/v#{version}/neko-v#{version}-darwin-aarch64.tar.gz"
-      sha256 "8be6405a37e5e6ea36ac841d4ccc9510e5de830ba1038466976dbd314745992d"
+      sha256 "b0ebc137cc343b3fc7a818a7fb2400e56909bf46a07f488cdaf196010d3ed174"
     end
   end
 
   on_linux do
     on_arm do
       url "https://github.com/superhq-ai/neko-computer/releases/download/v#{version}/neko-v#{version}-linux-aarch64.tar.gz"
-      sha256 "ada34055173dca135ac330122cc7f5d96b9df27eb6df70cef48ce353f0648edb"
+      sha256 "2328cae7c7a8c57e837ff66dac4074bb4061ffa5260afaaeb07878840f62a6a5"
     end
   end
 
